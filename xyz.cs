619643858fdb4197b82f2c4d33abf4a897f34764
LunaUI <59039458+LunaValerie@users.noreply.github.com>
2022-01-28 15:49:31 +0300

    Point Blank Box Esp X Y Z Add
    x,y,z eklenmistir

---
+++ xyz.cs
@@ -0,0 +1,12 @@
+  #region MEM
+        // LunaValerie#8497 ekleyin
+        string VIEWMATRIX = "PointBlank.exe+01BCB1DC,934,CA0,11C,264,C,A4"; // Matrix
+        string LOCALPLAYER = "PointBlank.exe+00F41618"; // LocalPlayer
+        int ENTITYBASE = 0x00F38730; // EntityBase // Ben Luna
+        string HP = ",0x36C"; //  Health Sart ( yanlıs olursa esp çizilmez)
+        string XYZ = ",0x14,290,28,19C,58,14,124,14,19C,58,14,11C"; //  X,Y,Z 
+        string TEAM = ",0x9c"; // SARK Değil ESP Çizilir
+        string DORMANT = ",0x17C"; // Dormant ( Sart Değil ESP Çizilir )
+        string GAME = "PointBlank.exe+"; // Game ( Çektiginiz Offsets neyse o)
+
+        #endregion
